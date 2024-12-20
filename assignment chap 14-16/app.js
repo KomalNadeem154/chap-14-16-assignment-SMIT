0b187972a4296a11cfc7fc2e8e43c5db5c4f8232
Komal Nadeem <koomalkhan111@gmail.com>
2024-12-20 13:24:20 +0500

    Add files via upload

---
+++ assignment chap 14-16/app.js
@@ -0,0 +1,106 @@
+// QUESTION NO:1
+
+// let stdname=[]
+
+// QUESTION NO:2
+
+// let studentname=[]
+
+// QUESTION NO:3
+
+// let stringarray=['Apple','Mango','Banana']
+
+// QUESTION No:4
+
+// let numarray=[5, 10, 15]
+
+// QUESTION NO:5
+
+// let booleanarray=['true','false','true']
+
+// QUESTION No:6
+
+// let mixed=['true',1,'apple',null]
+
+// QUESTION NO:7
+// document.write("Qualifications: <br> <br>")
+
+// let edu=[' 1) SSC <br>','2) HSC <br>','3) BCS <br>','4) BS <br>','5) BCOM <br>','6) MS <br>',' 7) M.Phil <br>',' 8) PhD']
+// document.write(edu)
+
+// QUESTION NO:8
+
+// function displayStudentResults() {
+//     let students = ["Michael", "John", "Tony"];
+//     let scores = [450, 380, 420]; 
+  
+//     let totalMarks = 500;
+
+//     console.log("Student Name  | Score | Percentage");
+//     console.log("------------------------------------");
+  
+//     for (let i = 0; i < students.length; i++) {
+      
+//       let percentage = (scores[i] / totalMarks) * 100;
+  
+//       console.log(`${students[i]}  | ${scores[i]}    | ${percentage.toFixed(2)}%`);
+//     }
+//   }
+  
+//   displayStudentResults();
+
+
+// QUESTION NO:9
+
+// let scores = [320, 230, 480, 120];
+
+// scores.sort(function(a, b) {
+//   return a - b;
+// });
+
+// console.log("Scores of Students: 320,230,480,120");
+// console.log("Ordered Scores of Students:", scores.join(','));
+
+// QUESTION N0:10
+
+// let cities=['Karachi','Kashmir','Islamabad','Quetta','Lahore'];
+// let selected=cities.slice(0,3);
+// console.log("Cities List:",cities);
+// console.log("Selected cities list:",selected);
+
+// QUESTION NO:11
+
+// let array=['This','is','my','cat']
+// let result=array.join('')
+// document.write(result)
+
+//QUESTION NO:13
+// let cities = ["keyboard", "mouse", "CPU"];
+// document.write("<h1>Devices:</h1>");
+// document.write(cities[0] + "<br>");
+// document.write(cities[1] + "<br>");
+// document.write(cities[2] + "<br>");
+
+//QUESTION NO:14
+// let cities = ["keyboard", "mouse", "CPU"];
+// document.write("<h1>Devices:</h1>");
+// document.write(cities[0] + "<br>");
+// document.write(cities[1] + "<br>");
+// document.write(cities[2] + "<br>");
+
+//QUESTION NO:15
+// let phonEs = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
+// document.write("<select>");
+// for (let i = 0; i < phonEs.length; i++) {
+//     document.write("<option value=>" + phonEs[i] + "</option>");
+// }
+// document.write("</select>");
+
+
+
+
+
+
+
+
+  
